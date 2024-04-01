c56f4788db4266a76c1f3c2b70694a5c0b1a7410
James Othniel <jamesothniel7@gmail.com>
2024-04-01 12:09:18 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 export default function App() {
   return (
-    <></>
-  )
+    <>
+      <div className="">
+        <p className="text-blue-500 text-3xl font-semibold">hello</p>
+      </div>
+    </>
+  );
 }
